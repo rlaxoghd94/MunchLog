f7864115136c6893be864db4ff89e0fd3cb2d6a4
Nicholas Taehong Kim <ktae.nic@gmail.com>
2019-04-22 22:35:27 +0900

    spring boot setup via intellij IDE

---
+++ Backend/munchlog/src/main/java/com/nictaehongkim/munchlog/MunchlogApplication.java
@@ -0,0 +1,13 @@
+package com.nictaehongkim.munchlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunchlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunchlogApplication.class, args);
+	}
+
+}
---
+++ Backend/munchlog/src/test/java/com/nictaehongkim/munchlog/MunchlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nictaehongkim.munchlog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MunchlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
